974d6281b30c4f7ae821c18e2cae4dbdc5d65f21
Matt Congdon <mattcong@users.noreply.github.com>
2026-02-08 23:29:00 +0000

    feat: series link page

---
+++ app/routes/series.tsx
@@ -0,0 +1,32 @@
+import { PageNav } from "~/components/PageNav/PageNav"
+import { Outlet } from "react-router"
+import { Page } from "~/components/Page/Page"
+import { Link } from "~/components/Link/Link"
+
+export default function Series() {
+  return (
+    <main>
+      <Page
+        pageNav={
+          <PageNav>
+            <Link to="a-nice-clean-game">A Nice Clean Game</Link>
+            <Link to="early-works">Early Works</Link>
+            <Link to="fields-of-blood">Field of Blood</Link>
+            <Link to="how-clean-my-country">How Clean my Country</Link>
+            <Link to="i-was-here">I Was Here</Link>
+            <Link to="lady-sings-the-blues">Lady Sings the Blues</Link>
+            <Link to="miscellaneous">Miscellaneous</Link>
+            <Link to="salome-and-the-boxers">Salome and the Boxers</Link>
+            <Link to="she-and-fella">She and Fella</Link>
+            <Link to="the-ballad-of-reading-gaol">The Ballad of Reading Gaol</Link>
+            <Link to="the-garden">The Garden</Link>
+            <Link to="the-ladies-of-the-night">The Ladies of the Night</Link>
+            <Link to="ulysses">Ulysses</Link>
+          </PageNav>
+        }
+      >
+        <Outlet />
+      </Page>
+    </main>
+  )
+}
